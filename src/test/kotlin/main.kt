b75586deb9b7db2f322d25f6ca56e5348a5ae0e9
Vladimir <belaevvova999@gmail.com>
2020-12-16 21:53:18 +0300

    Second commit

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,28 +1,24 @@
 fun main(){
-    val summ = 2000
+    var summ = 2000.0
     val meloman = true
-    fun sell() : Double {
-        val sell1 =
+        var sell =
             if(summ >= 1001 && summ <= 10000){
                 100.0
             }
-            else if(summ >= 10001){
-                val summ1 = summ - 100.0
-                summ1 * 0.05
+            else if(summ >= 10001 ){
+                summ - 100
+                summ * 0.05
             }
             else{
                 0.0
             }
-        return sell1
-    }
 
-    val resSumm = summ - sell()
+     summ -= sell
+
     if (meloman){
-        val sellM = resSumm * 0.01
-        val resSum1 = resSumm - sellM
-         println("Итоговая сумма к оплате: $resSum1")
+       sell = summ * 0.01
+        summ -= sell
+
     }
-    else{
-        println("Итоговая сумма к оплате: $resSumm")
-    }
+    println("Итоговая сумма к оплате: $summ")
 }
